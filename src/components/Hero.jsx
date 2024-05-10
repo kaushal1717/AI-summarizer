e3aa4d8d01d220d12a45bf5b61dee8748437fb08
kaushal1717 <kpadaliya1717@gmail.com>
2024-05-10 13:28:43 +0530

    created demo compoenent

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import { logo } from '../assets'
+
+function Hero() {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+        <nav className="flex justify-between items-center w-full mb-10 pt-3">
+            <img src={logo} alt="App logo" className="w-28 object-contain"/>
+            <button type="button" onClick={()=>window.open('https://github.com/kaushal1717/AI-summarizer')} className='black_btn'> GitHub </button>
+        </nav>
+        <h1 className='head_text'>
+            Summarize Articles with  <br className="max-md:hidden"/>
+            <span className='orange_gradient'>OPENAI GPT-4</span>
+        </h1>
+        <h2 className='desc'>
+            Simplify your reading with Summize, an open-source article summarizer that transforms lengthy articles into clear and consice summaries
+        </h2>
+    </header>
+  )
+}
+
+export default Hero
